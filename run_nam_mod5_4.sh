caf55fa860ece6a15f69bf49acd177ef726a72de
xumingkuan <xumingkuan0721@126.com>
2022-04-09 12:41:19 -0400

    nam3

--- run_nam_mod5_4.sh
+++ run_nam_mod5_4.sh
@@ -1,5 +1,6 @@
 cd build
 make test_nam
-./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam/mod5_4.qasm.output.43.nam --eqset ../Nam_4_3_complete_ECC_set.json &
-./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam/mod5_4.qasm.output.53.nam --eqset ../Nam_5_3_complete_ECC_set.json &
-./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam/mod5_4.qasm.output.63.nam --eqset ../Nam_6_3_complete_ECC_set.json &
+./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam3/mod5_4.qasm.output.33.nam --eqset ../Nam_3_3_complete_ECC_set.json &
+./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam3/mod5_4.qasm.output.43.nam --eqset ../Nam_4_3_complete_ECC_set.json &
+./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam3/mod5_4.qasm.output.53.nam --eqset ../Nam_5_3_complete_ECC_set.json &
+./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam3/mod5_4.qasm.output.63.nam --eqset ../Nam_6_3_complete_ECC_set.json &
