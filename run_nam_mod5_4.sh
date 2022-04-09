72b5a539d6eb245b67e7ceb142af320dac9d26cb
xumingkuan <xumingkuan0721@126.com>
2022-04-09 12:42:11 -0400

    Add 73

--- run_nam_mod5_4.sh
+++ run_nam_mod5_4.sh
@@ -4,3 +4,4 @@ make test_nam
 ./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam3/mod5_4.qasm.output.43.nam --eqset ../Nam_4_3_complete_ECC_set.json &
 ./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam3/mod5_4.qasm.output.53.nam --eqset ../Nam_5_3_complete_ECC_set.json &
 ./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam3/mod5_4.qasm.output.63.nam --eqset ../Nam_6_3_complete_ECC_set.json &
+./test_nam ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/nam3/mod5_4.qasm.output.73.nam --eqset ../Nam_7_3_complete_ECC_set.json &
