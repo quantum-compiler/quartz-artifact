bd27c520a8f7a96bdcfd6d369429b295564427c9
xumingkuan <xumingkuan0721@126.com>
2022-04-10 17:54:39 -0400

    rigetti 2

--- src/test/gen_ecc_set.cpp
+++ src/test/gen_ecc_set.cpp
@@ -84,11 +84,11 @@ void gen_ecc_set(const std::vector<GateType> &supported_gates,
 }
 
 int main() {
-  for (int n = 3; n <= 6; n++) {
-    std::string file_prefix = "Rigetti_const_neg_";
+  for (int n = 2; n <= 2; n++) {
+    std::string file_prefix = "Rigetti_const_";
     file_prefix += std::to_string(n);
     file_prefix += "_3_";
-    gen_ecc_set({GateType::rx1, GateType::x, GateType::rx3, GateType::rz, GateType::cz, GateType::add, GateType::neg},
+    gen_ecc_set({GateType::rx1, GateType::x, GateType::rx3, GateType::rz, GateType::cz, GateType::add},
                 file_prefix, true, 3, 2, n);
   }
   return 0;
